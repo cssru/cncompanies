431e0b2a9df25efd2d9949dca2e1da14d62ecef9
cssru <cssru@mail.ru>
2015-09-29 15:37:13 +0300

    CompanyService complete

--- src/main/java/com/cssru/cncompanies/service/CompanyService.java
+++ src/main/java/com/cssru/cncompanies/service/CompanyService.java
@@ -1,8 +1,6 @@
 package com.cssru.cncompanies.service;
 
-import com.cssru.cncompanies.domain.Account;
 import com.cssru.cncompanies.domain.Company;
-import com.cssru.cncompanies.domain.Human;
 import com.cssru.cncompanies.dto.CompanyDto;
 import com.cssru.cncompanies.exception.AccessDeniedException;
 
@@ -10,8 +8,8 @@ import java.util.List;
 
 public interface CompanyService {
 	void add(CompanyDto company) throws AccessDeniedException;
-	List<CompanyDto> list(Account holder) throws AccessDeniedException;
-	List<CompanyDto> list(Human manager) throws AccessDeniedException;
+	List<CompanyDto> list() throws AccessDeniedException;
+	List<CompanyDto> list(Long managerId) throws AccessDeniedException;
 	void delete(Long id) throws AccessDeniedException;
 	void update(CompanyDto company) throws AccessDeniedException;
 	Company get(Long id) throws AccessDeniedException;
